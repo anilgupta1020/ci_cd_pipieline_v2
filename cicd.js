2c7f6d20f9bcf894acf61cec24aae4244b398a4b
AnilGupta17 <anilgupta01it@gmail.com>
2021-12-26 17:39:27 +0530

    update file

--- cicd.js
+++ cicd.js
@@ -38,4 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(process.env.PORT || '3030', () => {
+	console.log(`Server is running on port: ${process.env.PORT || '3030'}`);
+});
